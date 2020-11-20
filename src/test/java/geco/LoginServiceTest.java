746fa11b075e70215440e0d8235e421a1c8b27e7
Tom <tom.meehan@etud.univ-jfc.fr>
2020-11-20 08:59:36 +0100

    Add 100% coverage LoginService tests

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -2,21 +2,37 @@ package geco;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+
 public class LoginServiceTest extends TestCase {
+
+    private LoginService loginService;
+
 
     public void setUp() throws Exception {
         super.setUp();
+        String[] logins = {"login1","login2","login3"};
+        loginService = new LoginService(logins);
     }
 
     public void testLoginExists() {
+        assertTrue(loginService.loginExists("login1"));
+        assertFalse(loginService.loginExists("login4"));
     }
 
     public void testAddLogin() {
+        loginService.addLogin("login4");
+        assertTrue(loginService.loginExists("login4"));
     }
 
     public void testFindAllLoginsStartingWith() {
+        assertTrue(loginService.findAllLoginsStartingWith("nigol").isEmpty());
+        assertFalse(loginService.findAllLoginsStartingWith("log").isEmpty());
     }
 
     public void testFindAllLogins() {
+        List<String> logins = loginService.findAllLogins();
+        assertFalse(logins.isEmpty());
+        assertEquals("login1",logins.get(0));
     }
 }
