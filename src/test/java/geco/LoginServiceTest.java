0f6b91585de98e98b274d299ebbb4e236430ef0e
Tom <tom.meehan@etud.univ-jfc.fr>
2020-11-20 08:36:15 +0100

    Add LoginServiceTest Remove AppTest

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,22 @@
+package geco;
+
+import junit.framework.TestCase;
+
+public class LoginServiceTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testLoginExists() {
+    }
+
+    public void testAddLogin() {
+    }
+
+    public void testFindAllLoginsStartingWith() {
+    }
+
+    public void testFindAllLogins() {
+    }
+}
